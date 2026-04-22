e577fc12cd584c6307547aef9e8ce307e5f79ee6
athenaz14 <athenaz11@nycstudents.net>
2026-04-22 09:23:48 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,39 @@
+class Calculator():
+    def add(x, y):
+        print(x + y)
+        return x + y
+
+    def add_many(numbers):
+        print(sum(numbers))
+        return sum(numbers)
+
+    def subtract(numbers):
+        return numbers
+
+Calculator.add(5, 6)
+
+class Hero:
+    def __init__(self, name, money, inventory):
+        self.name = name
+        self.money = money
+        self.inventory = inventory
+
+    def buy(self, item):
+        self.inventory.append(item)
+        print(self.inventory)
+Jillian = Hero("Jillian", 150, ["Potion"])
+Jillian.buy({"title": "Sword", "atk": 34})
+print(Jillian.__dict__)
+
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance  # double underscore means "private"
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def show_balance(self):
+        print(f"{self.owner} has ${self.__balance}")
+
+Jillian =BankAccount("deposit", "show_balance")
